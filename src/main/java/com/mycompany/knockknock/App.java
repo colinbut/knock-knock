d4aa0b150d7b5890cca9d862923af6a821c3e471
Colin But <colinbut@users.noreply.github.com>
2016-04-17 20:58:13 +0100

    Added initial project folder

---
+++ src/main/java/com/mycompany/knockknock/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.knockknock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
